a92fc56454c0b63afc79cd23b78a3a50b84e6a2b
Rohan Hitchcock <hitchcock.rohan@gmail.com>
2020-05-22 14:59:51 +1000

    computing dot2vecs for more dimensions using splits of the supplied training data

---
+++ ass2/generate_docvecs.py
@@ -0,0 +1,89 @@
+""" Most of this code is adapted from the supplied features.ipynb file"""
+
+import numpy as np
+import pandas as pd
+import gensim
+from sklearn.model_selection import train_test_split
+import os
+
+RANDOM_STATE = 7
+
+
+def get_dot2vec_split(dim):
+    """ For loading the precomputed train-test split of doc2vec vectors of the 
+        specified dimension"""
+    split_dir = f"d2v_strat{dim}"
+
+    Xtrain = pd.read_csv(f"./datasets/computed/{split_dir}/Xtrain.csv", index_col=0)
+    Xtest = pd.read_csv(f"./datasets/computed/{split_dir}/Xtest.csv", index_col=0)
+    ytest = pd.read_csv(f"./datasets/computed/{split_dir}/ytest.csv", delimiter=',', header=None)
+    ytrain = pd.read_csv(f"./datasets/computed/{split_dir}/ytrain.csv", delimiter=',', header=None)
+
+    return Xtrain, Xtest, ytrain[1], ytest[1]
+
+
+# function to preprocess and tokenize text
+def tokenize_corpus(txt, tokens_only=False):
+    for i, line in enumerate(txt):
+        tokens = gensim.utils.simple_preprocess(line)
+        if tokens_only:
+            yield tokens
+        else:
+            yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+
+
+def compute_doc_embedding(train_corpus, test, dimension):
+
+    # tokenize a training corpus
+    corpus = list(tokenize_corpus(train_corpus))
+
+    # train doc2vec on the training corpus
+    model = gensim.models.doc2vec.Doc2Vec(vector_size=dimension, min_count=2, epochs=40)
+    model.build_vocab(corpus)
+    model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+
+    # tokenize new documents
+    doc_test = list(tokenize_corpus(test, tokens_only=True))
+
+    # generate embeddings for the new documents
+    doc_test_emb = np.zeros((len(doc_test),dimension))
+    for i in range(len(doc_test)):
+        doc_test_emb[i,:] = model.infer_vector(doc_test[i])
+
+    # tokenize new documents
+    doc_train = list(tokenize_corpus(train_corpus, tokens_only=True))
+
+    # generate embeddings for the new documents
+    doc_train_emb = np.zeros((len(doc_train),dimension))
+    for i in range(len(doc_train)):
+        doc_train_emb[i,:] = model.infer_vector(doc_train[i])
+
+    return doc_test_emb, doc_train_emb
+
+#MAIN
+
+res_train_review_table = pd.read_csv(r"./datasets/review_text_train.csv", index_col = False, delimiter = ',', header=0)
+data = res_train_review_table['review']
+
+data_meta = pd.read_csv(r"./datasets/review_meta_train.csv")
+class_label = data_meta['rating']
+
+for dim in range(25, 301, 25):
+    print(f"running for dim {dim}")
+
+    X_train, X_test, y_train, y_test= train_test_split(data, class_label, test_size=0.2, random_state=RANDOM_STATE*dim, stratify=class_label)
+
+    test_embedding, train_embedding = compute_doc_embedding(X_train, X_test, dim)
+    
+    os.makedirs(f"./datasets/computed/d2v_strat{dim}/")
+
+    pd.DataFrame(test_embedding).to_csv(f"./datasets/computed/d2v_strat{dim}/Xtest.csv")
+    pd.DataFrame(train_embedding).to_csv(f"./datasets/computed/d2v_strat{dim}/Xtrain.csv")
+    y_train
+
+    y_train.to_csv(f"./datasets/computed/d2v_strat{dim}/ytrain.csv")
+    y_test.to_csv(f"./datasets/computed/d2v_strat{dim}/ytest.csv")
+
+    del test_embedding
+    del train_embedding
+
--- ass2/svm.py
+++ ass2/svm.py
@@ -3,45 +3,200 @@ from sklearn.model_selection import KFold
 from sklearn import metrics
 import numpy as np
 import pandas as pd
+from collections import namedtuple
+import matplotlib.pyplot as plt
 
-data = pd.read_csv(
+from generate_docvecs import get_dot2vec_split
+
+Evaluation = namedtuple("Evaluation", ["accuracy", "fscore", "precision", "recall"])
+
+def evaluate_model(X_file, y, model, num_xval_splits=3):
+    """ Evaluate a model on a dataset (X, y) using cross validaiton.
+
+        Args:
+            X: instances (pd.DataFrame)
+            y: class labels (pd.DataFrame or pd.Series)
+            model: an instance of a class with methods .fit and .predict
+            num_xval_splits: the number of cross validaiton splits to use
+
+        Returns: A tuple of fscore, accuracy, precision and recall averaged over
+        the cross validaiton splits
+    """
+    X = pd.read_csv(X_file, index_col=False, delimiter=',', header=None
+    )
+    cross_val = KFold(n_splits=num_xval_splits)
+
+    fscore_tst = np.empty(num_xval_splits)
+    accuracy_tst = np.empty(num_xval_splits)
+    precision_tst = np.empty(num_xval_splits)
+    recall_tst =  np.empty(num_xval_splits)
+
+    fscore_trn = np.empty(num_xval_splits)
+    accuracy_trn = np.empty(num_xval_splits)
+    precision_trn = np.empty(num_xval_splits)
+    recall_trn = np.empty(num_xval_splits)
+
+    for i, split in enumerate(cross_val.split(X)):
+        train_i, test_i = split
+
+        data_train, data_test = X.iloc[train_i], X.iloc[test_i]
+        class_label_train, class_label_test = y.iloc[train_i], y.iloc[test_i]
+        
+        model.fit(data_train, class_label_train)
+
+        #prediction
+        predict_label = model.predict(data_test)
+
+        #evaluation
+        fscore_tst[i] = metrics.f1_score(class_label_test, predict_label, average='weighted')
+        accuracy_tst[i] = metrics.accuracy_score(class_label_test, predict_label)
+        precision_tst[i] = metrics.precision_score(class_label_test, predict_label, average='weighted')
+        recall_tst[i] = metrics.recall_score(class_label_test, predict_label, average='weighted')
+
+        predict_label = model.predict(data_train)
+
+        #evaluation
+        fscore_trn[i] = metrics.f1_score(class_label_train, predict_label, average='weighted')
+        accuracy_trn[i] = metrics.accuracy_score(class_label_train, predict_label)
+        precision_trn[i] = metrics.precision_score(class_label_train, predict_label, average='weighted')
+        recall_trn[i] = metrics.recall_score(class_label_train, predict_label, average='weighted')
+
+    del X
+
+    return (
+        Evaluation(np.mean(accuracy_tst), np.mean(fscore_tst), np.mean(precision_tst), np.mean(recall_tst)), 
+        Evaluation(np.mean(accuracy_trn), np.mean(fscore_trn), np.mean(precision_trn), np.mean(recall_trn))
+    ) 
+
+
+def feature_dim_learning_curve(feature_sets, class_label, model, x_vals, y_metric_name, y_label, title):
+
+    y_vals_test = []
+    y_vals_train = []
+    for i, X in enumerate(feature_sets):
+
+        print(f"run {i}")
+
+        test_eval, train_eval = evaluate_model(X, class_label, model)
+
+        y_vals_test.append(getattr(test_eval, y_metric_name))
+        y_vals_train.append(getattr(train_eval, y_metric_name))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.plot(x_vals, y_vals_test, 'bo', linestyle='-', label='Test')
+    ax.plot(x_vals, y_vals_test, 'go', linestyle='-', label='Train')
+    
+    ax.set_xaxis('Feature space dimension')
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+
+    ax.legend(loc='upper left', title_fontsize=12)
+
+    plt.show()
+
+
+
+
+"""
+data50 = pd.read_csv(
     r"./datasets/review_text_features_doc2vec50/review_text_train_doc2vec50.csv", 
     index_col=False, delimiter=',', header=None
 )
+
+data100 = pd.read_csv(
+    r"./datasets/review_text_features_doc2vec100/review_text_train_doc2vec100.csv", 
+    index_col=False, delimiter=',', header=None
+)
+
+data200 = pd.read_csv(
+    r"./datasets/review_text_features_doc2vec200/review_text_train_doc2vec200.csv", 
+    index_col=False, delimiter=',', header=None
+)
+"""
+"""
+data50 =  r"./datasets/review_text_features_doc2vec50/review_text_train_doc2vec50.csv"
+data100 = r"./datasets/review_text_features_doc2vec100/review_text_train_doc2vec100.csv"
+data200 = r"./datasets/review_text_features_doc2vec200/review_text_train_doc2vec200.csv"
+
+"""
+
 data_meta = pd.read_csv(r"./datasets/review_meta_train.csv")
 class_label = data_meta['rating']
 
-#specify the model here
-model = svm.SVC(C=1, kernel='sigmoid')
 
-cross_val = KFold(n_splits=5)
+print("original dataset")
+num_instances = len(class_label)
+one_count = np.count_nonzero(class_label == 1)
+three_count = np.count_nonzero(class_label == 3)
+five_count = np.count_nonzero(class_label == 5)
+print(f"Count of 1: {one_count} ({round(100 * one_count / num_instances, 3)} pc)")
+print(f"Count of 3: {three_count} ({round(100 * three_count / num_instances, 3)} pc)")
+print(f"Count of 5: {five_count} ({round(100 * five_count / num_instances, 3)} pc)")
+print(f"Total: {num_instances}")
 
-fscore = []
-accuracy = []
-precision = []
-recall = []
+print("-------------------")
+for dim in range(25, 301, 25):
 
-for train_i, test_i in cross_val.split(data):
+    print(f"for dimension {dim}")
+    Xtrain, Xtest, ytrain, ytest = get_dot2vec_split(dim)
+
+    print("train:")
+    num_instances = len(ytrain)
+    one_count = np.count_nonzero(ytrain == 1)
+    three_count = np.count_nonzero(ytrain == 3)
+    five_count = np.count_nonzero(ytrain == 5)
+
+    print(f"Count of 1: {one_count} ({round(100 * one_count / num_instances, 3)} pc)")
+    print(f"Count of 3: {three_count} ({round(100 * three_count / num_instances, 3)} pc)")
+    print(f"Count of 5: {five_count} ({round(100 * five_count / num_instances, 3)} pc)")
+    print(f"Total: {num_instances}")
+
+    print("test:")
+    num_instances = len(ytest)
+    one_count = np.count_nonzero(ytest == 1)
+    three_count = np.count_nonzero(ytest == 3)
+    five_count = np.count_nonzero(ytest == 5)
+
+    print(f"Count of 1: {one_count} ({round(100 * one_count / num_instances, 3)} pc)")
+    print(f"Count of 3: {three_count} ({round(100 * three_count / num_instances, 3)} pc)")
+    print(f"Count of 5: {five_count} ({round(100 * five_count / num_instances, 3)} pc)")
+    print(f"Total: {num_instances}")
+
+    print("-------------------")
 
 
-    data_train, data_test = data.iloc[train_i], data.iloc[test_i]
-    class_label_train, class_label_test = class_label.iloc[train_i], class_label.iloc[test_i]
 
-    model.fit(data_train, class_label_train)
+"""
+kernal = 'linear'
+model = svm.SVC(C=1, kernel=kernal)
 
-    #prediction
-    predict_label = model.predict(data_test)
+feature_sets = [data50, data100, data200]
+x_vals = [50, 100, 200]
 
+y_metric_name = 'fscore'
+y_label = "F-Score"
+title = "Linear SVM"
 
-    #evaluation
-    #fscore.append(metrics.f1_score(class_label_test, predict_label))
-    accuracy.append(metrics.accuracy_score(class_label_test, predict_label))
-    #precision.append(metrics.precision_score(class_label_test, predict_label))
-    #recall.append(metrics.recall_score(class_label_test, predict_label))
+feature_dim_learning_curve(feature_sets, class_label, model, x_vals, y_metric_name, y_label, title)
+"""
 
+"""
+data = data50
+data_meta = pd.read_csv(r"./datasets/review_meta_train.csv")
+class_label = data_meta['rating']
 
-print(f"Accuracy: {np.mean(accuracy)}\n"
-      f"Precision: {np.mean(precision)}\n"
-      f"Recall: {np.mean(recall)}\n"
-      f"F1Score: {np.mean(fscore)}")
+for kernal in ['linear', 'poly', 'rbf', 'sigmoid']:
+    
+    model = svm.SVC(C=1, kernel=kernal)
 
+    eval_test, eval_train = evaluate_model(data, class_label, model)
+    print(kernal)
+    print(f"Accuracy: {eval_test.accuracy}\n"
+          f"Precision: {eval_test.precision}\n"
+          f"Recall: {eval_test.recall}\n"
+          f"F1Score: {eval_test.fscore}")
+
+    print()
+"""
